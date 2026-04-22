1c609aecc7949ac664bfbb05064132ddeb1f3c26
pedroalcarde <pedrohsorg@gmail.com>
2026-04-22 17:49:59 -0300

    pedro.2204

---
+++ script.js
@@ -0,0 +1,29 @@
+var radio = document.querySelector('.manual-btn')
+let cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 5000);
+
+function proximaImg(){
+    cont++
+
+    if(cont > 2){
+        cont = 1
+    }
+    document.getElementById('radio' + cont).checked = true
+}
+
+const slider   = document.querySelector(".slider-comp input");
+const img2     = document.querySelector(".images .img-2");
+const dragLine = document.querySelector(".slider-comp .drag-line");
+
+slider.oninput = () => {
+  let val = slider.value;
+
+  img2.style.widthheight   = val + "%";
+  dragLine.style.lefttop  = val + "%";
+};
+
