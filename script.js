de0a2a1922e1b5268ff4d6272b1f35165076707f
pedroalcarde <pedrohsorg@gmail.com>
2026-04-23 17:58:09 -0300

    pedro2304alteracoes

--- script.js
+++ script.js
@@ -27,3 +27,19 @@ slider.oninput = () => {
   dragLine.style.lefttop  = val + "%";
 };
 
+
+new Swiper('.card-wrapper', {
+
+  loop: true,
+
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
