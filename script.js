8ceeb70c09a556b885d65bdaf5f0f4ffe2d1ba19
pedroalcarde <pedrohsorg@gmail.com>
2026-04-28 14:00:53 -0300

    atualizacaojs

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// Carrossel promoçoes
-var radio = document.querySelector('.manual-btn')
-let cont = 1
-
-document.getElementById('radio1').checked = true
-
-setInterval(() => {
-    proximaImg()
-}, 5000);
-
-function proximaImg(){
-    cont++
-
-    if(cont > 2){
-        cont = 1
-    }
-    document.getElementById('radio' + cont).checked = true
-}
-
-
 // Menu hamburguer
 function clickMenu() {
     let itens = document.getElementById('itens');
@@ -28,17 +8,4 @@ function clickMenu() {
     }
 }
 
-// carrossel depoimentos mobile
-var swiper = new Swiper(".swiper", {
-    loop: true, 
-    
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true
-      },
-    });
-  
+
